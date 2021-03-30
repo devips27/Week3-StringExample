43249092442b45819e5a870da49f84c2ec49fcd4
haseeb1988 <haseeb.dev1988@gmail.com>
2021-03-30 15:50:33 -0400

    Setup String Excercise and add all files

---
+++ src/com/Package/myString/StringDemo.java
@@ -0,0 +1,42 @@
+package com.Package.myString;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			String s = "Welcome to Java";
+			
+			String ss ="Welcome to Java";
+			
+			System.out.println(s.replace("Java", "python"));
+			
+			
+			//String a = s + i; 
+			 String a = s.concat(ss);  
+	          System.out.println(a + "\n ");
+	         
+	         System.out.println(a.charAt(10));
+	         
+	         System.out.println(ss.substring(5,15));
+			
+	         System.out.println( " position of G is: "+ss.indexOf('G'));
+	         
+	          System.out.println(ss.toUpperCase());
+	          
+	          int l = ss.length();
+	          
+	          
+	          System.out.println(l);
+	          
+	          String c = "haseeb";
+	          
+	          boolean  b = c.isEmpty();
+	          System.out.println(b);
+	          
+	          System.out.println(ss.trim());
+	          boolean  x =  s.equals(ss);
+	          System.out.println(x);
+	          
+	}
+
+}
